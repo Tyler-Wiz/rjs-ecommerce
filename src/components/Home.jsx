fea6ce6033b06f1456a30918c873209051aa6f3b
tyler <tylertooxclusive@gmail.com>
2021-11-27 11:54:08 +0000

    sturcture

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Announcement } from './Announcement'
+
+export const Home = () => {
+    return (
+        <div>
+            <Announcement/>
+        </div>
+    )
+}
